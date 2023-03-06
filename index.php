9c6bb34793c836cfdfd94bc40554b2efe79e11fd
Alexander <formysite.ru@yandex.ru>
2023-03-06 15:58:54 +0300

    begin

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Урок 7.7. SQL запросы. INNER JOIN</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+</head>
+
+<body>
+    <main>
+        <header>
+            <h1>
+                Урок 7.7. SQL запросы. INNER JOIN
+            </h1>
+            <h4 class="subtitle">
+                Урок 7.7. SQL запросы. INNER JOIN
+            </h4>
+            <div class="code-php">
+                <?php
+                $nickname = "Александр!";
+                $hello = "Привет ";
+                $offer = "Продолжаем изучение РНР и MySQL<br>
+                            Узнаем как можно SQL запросом обратиться сразу двум таблицам и рассмотрим операторы INNER JOIN";
+
+                echo "<h2 class='subtitle-php'>" . $hello . $nickname . "<br>" . "<br>" . $offer;
+                ?>
+            </div>
+        </header>
+
+    </main>
+
+</body>
+
+</html>
